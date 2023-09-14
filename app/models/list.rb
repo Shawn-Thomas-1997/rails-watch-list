39af32bf5377a6c00710244b234054700b50e4fe
Shawn Thomas <shawnt41@gmail.com>
2023-09-14 16:56:30 +0200

    Created a watch list

---
+++ app/models/list.rb
@@ -0,0 +1,11 @@
+class List < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  has_many :bookmarks, dependent: :destroy
+  has_many :movies, through: :bookmarks, dependent: :destroy
+
+  private
+
+  def saved_movies
+    bookmarks.map(&:movie)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+require "json"
+require "rest-client"
+puts "Cleaning database..."
+Movie.destroy_all
+api_key = "<your_api_key>"
+response = RestClient.get "http://tmdb.lewagon.com/movie/top_rated?api_key=#{api_key}"
+data = JSON.parse(response.body)
+image_api = "https://image.tmdb.org/t/p/w500"
+puts "Creating fake movies..."
+data["results"].each do |movie_data|
+  title = movie_data["original_title"]
+  overview = movie_data["overview"]
+  poster_url = "#{image_api}#{movie_data['poster_path']}"
+  rating = movie_data["vote_average"]
+  Movie.create(title: title, overview: overview, poster_url: poster_url, rating: rating)
+end
+puts "Finished!"
